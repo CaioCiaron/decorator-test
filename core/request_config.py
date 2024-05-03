622844ed4a725d4f9d9a03832eb7c6d8a8732cf2
CaioCiaron <caiociaron@gmail.com>
2024-05-03 00:01:01 -0300

    renaming methods

--- core/request_config.py
+++ core/request_config.py
@@ -11,22 +11,22 @@ class RequestConfig:
 
   @classmethod
   def Get(cls, url: str):
-    return cls._decorator_wrapper(Method.GET, url)
+    return cls._register_method(Method.GET, url)
 
   @classmethod
   def Post(cls, url: str):
-    return cls._decorator_wrapper(Method.POST, url)
+    return cls._register_method(Method.POST, url)
   
   @classmethod
   def Put(cls, url: str):
-    return cls._decorator_wrapper(Method.PUT, url)
+    return cls._register_method(Method.PUT, url)
   
   @classmethod
-  def _decorator_wrapper(cls, http_method: Method, url: str):
-    return lambda func: cls._register_method(http_method, url, func)
+  def _register_method(cls, http_method: Method, url: str):
+    return lambda func: cls._decorator_wrapper(http_method, url, func)
 
   @classmethod
-  def _register_method(cls, http_method: Method, url: str, func: Callable):
+  def _decorator_wrapper(cls, http_method: Method, url: str, func: Callable):
     print(f'Registering {http_method} for URL {url}')
     if url not in cls._mappings[Method(http_method)]:
       cls._mappings[Method(http_method)][url] = func
